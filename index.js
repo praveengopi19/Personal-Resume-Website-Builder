f7073a5b4cb890596781f65cf70d674169107d37
Praveen Kumar <praveengopi16@gmail.com>
2020-09-18 11:31:13 +0530

    html string completed

--- index.js
+++ index.js
@@ -96,76 +96,226 @@ var html = `<!DOCTYPE html>
 
 function submitform() {
 
-    html = html + `<title>Resume | ${formvalues["name"].value}</title>
+    html = html + `
+    <title>Resume | ${formvalues["name"].value}</title>
     </head>
 
 <body>
     <div class="left">
-        <div class="leftbg">
-        </div>
+        <div class="leftbg"></div>
         <div class="left_inner">
-        <div class="left_info">
+            <div class="left_info">
         `;
 
     if (formvalues["name"].value && formvalues["name"].value.trim().length) {
         html = html + `
-            <h2> Hey, I'm ${formvalues["name"].value}
-            </h2>`
+                <h2> Hey, I'm ${formvalues["name"].value}</h2>`
     }
 
     if (formvalues["tagline"].value && formvalues["tagline"].value.trim().length) {
-        html = html + `<div>
+        html = html + `
+        <div>
             ${formvalues["tagline"].value}
         </div>`
     }
 
-    html = html + `<div class="links">`
+    html = html + `
+                <div class="links">`
 
     if (formvalues["mailid"].value && formvalues["mailid"].value.trim().length) {
-        html = html + `<a href="mailto:${formvalues["mailid"].value}" target="_blank">
+        html = html + `
+        <a href="mailto:${formvalues["mailid"].value}" target="_blank">
             <i class="fa fa-envelope"></i>
         </a>`
     }
 
-    if (formvalues["linedin"].value && formvalues["linkedin"].value.trim().length) {
-        html = html + `<a href="https://www.linkedin.com/in/${formvalues["linedin"].value}" target="_blank">
-        <i class="fa fa-linkedin"></i>
-    </a>`
+    if (formvalues["linkedin"].value && formvalues["linkedin"].value.trim().length) {
+        html = html + `
+        <a href="https://www.linkedin.com/in/${formvalues["linkedin"].value}" target="_blank">
+            <i class="fa fa-linkedin"></i>
+        </a>`
     }
 
     if (formvalues["github"].value && formvalues["github"].value.trim().length) {
-        html = html + `<a href="https://www.linkedin.com/in/${formvalues["github"].value}" target="_blank">
-        <i class="fa fa-github"></i>
-    </a>`
+        html = html + `
+        <a href="https://www.linkedin.com/in/${formvalues["github"].value}" target="_blank">
+            <i class="fa fa-github"></i>
+        </a>`
     }
 
-    if (formvalues["linedin"].value && formvalues["linkedin"].value.trim().length) {
-        html = html + `<a href="https://www.linkedin.com/in/${formvalues["linedin"].value}" target="_blank">
-        <i class="fa fa-linkedin"></i>
-    </a>`
+    if (formvalues["portfolio"].value && formvalues["portfolio"].value.trim().length) {
+        html = html + `
+        <a href="${formvalues["portfolio"].value}" target="_blank">
+            <i class="fa fa-globe"></i>
+        </a>`
     }
 
-    if (formvalues["linedin"].value && formvalues["linkedin"].value.trim().length) {
-        html = html + `<a href="https://www.linkedin.com/in/${formvalues["linedin"].value}" target="_blank">
-        <i class="fa fa-linkedin"></i>
-    </a>`
+    if (formvalues["twitter"].value && formvalues["twitter"].value.trim().length) {
+        html = html + `
+        <a href="https://www.linkedin.com/in/${formvalues["linkedin"].value}" target="_blank">
+            <i class="fa fa-linkedin"></i>
+        </a>`
     }
 
+    html = html + `
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="right">
+        <div class="right_inner">`
+
+    if (formvalues["about"].value && formvalues["about"].value.trim().length) {
+        html = html + `
+        <div class="content">
+        <h4 class="heading">INTRO</h4>
+        <p>
+        ${formvalues["about"].value}
+        </p>
+    </div>`
+    }
+
+    html = html + `
+    <!-- Skills -->
+    <div class="content">
+                <h4 class="heading">SKILLS</h4>`
+
+    let skillcheck = false;
     for (let i = 1; i <= skills; i++) {
-        console.log(formvalues[`skill[${i}][heading]`].value);
+        let tempvalue = formvalues[`skill[${i}][value]`].value;
+        let tempheading = formvalues[`skill[${i}][heading]`].value;
+        if (tempvalue && tempvalue.trim().length) {
+            skillcheck = true;
+            html = html + `
+            <div class="category">
+            ${tempheading && tempheading.trim().length ? `<strong> ${tempheading} </strong>` : ""}
+            <span>${tempvalue}</span>
+        </div>`
+        }
     }
+
+    html = html + `
+    </div>`
+
+    if (skillcheck == false) {
+        html = html.split("<!-- Skills -->")[0];
+    }
+
+    html = html + `
+    <!-- Education -->
+    <div class="content">
+                <h4 class="heading">EDUCATION</h4>`
+    let educationcheck = false;
 
     for (let i = 1; i <= education; i++) {
-        console.log(formvalues[`skill[${i}][heading]`].value);
+        let tempuniversity = formvalues[`education[${i}][university]`].value;
+        let tempcourse = formvalues[`education[${i}][course]`].value;
+        let tempfrom = formvalues[`education[${i}][from]`].value;
+        let tempcgpa = formvalues[`education[${i}][cgpa]`].value;
+
+        if (tempuniversity && tempuniversity.trim().length) {
+            educationcheck = true;
+            html = html + `
+                <div class="category">
+                    <div>
+                        <strong>${tempuniversity}</strong>
+                    </div>
+                </div>
+                <div class="category">
+                    ${tempcourse}
+                    <div>
+                        <div class="duration">
+                            ${tempfrom}
+                            ${tempcgpa && tempcgpa.trim().length ? `<div style="float: right;">
+                            ${tempcgpa}
+                        </div>`: ``}
+                        </div>
+                    </div>
+                </div>`
+        }
     }
+
+    html = html + `
+    </div>`;
+
+    if (educationcheck == false) {
+        html = html.split("<!-- Education -->")[0];
+    }
+
+    html = html + `
+    <!-- Experience -->
+    <div class="content">
+                <h4 class="heading">EXPERIENCE</h4>`
+
+    let experiencecheck = false;
 
     for (let i = 1; i <= experience; i++) {
-        console.log(formvalues[`skill[${i}][heading]`].value);
+        let tempcompany = formvalues[`experience[${i}][companyname]`].value;
+        let temprole = formvalues[`experience[${i}][role]`].value;
+        let tempdescription = formvalues[`experience[${i}][description]`].value;
+        let tempduration = formvalues[`experience[${i}][duration]`].value;
+
+        if (tempcompany && tempcompany.trim().length) {
+            experiencecheck = true;
+            html = html + `
+                <div class="category">
+                    <div>
+                        <strong>${tempcompany}</strong>
+                    </div>
+                    <div>
+                        <strong>${temprole}</strong>
+                    </div>
+                    <div class="duration">
+                        <strong>${tempduration}</strong>
+                    </div>
+                    ${tempdescription}
+                </div>`
+        }
     }
 
-    for (let i = 1; i <= projects; i++) {
-        console.log(formvalues[`skill[${i}][heading]`].value);
+    html = html + `
+    </div>`;
+
+    if (experiencecheck == false) {
+        html = html.split("<!-- Experience -->")[0];
     }
+
+    html = html + `
+    <!-- Projects -->
+    <div class="content">
+                <h4 class="heading">PROJECTS</h4>`
+
+    let projectcheck = false;
+
+    for (let i = 1; i <= projects; i++) {
+        let tempname = formvalues[`project[${i}][name]`].value;
+        let tempdescription = formvalues[`project[${i}][description]`].value;
+
+        if (tempname && tempname.trim().length) {
+            projectcheck = true;
+            html = html +
+                `<div class="category">
+                <div class="subheading">
+                    <strong>${tempname}</strong>
+                </div>
+                    <p>${tempdescription}</p>
+                </div>`
+        }
+    }
+
+    html = html + `
+    </div>`;
+
+    if (projectcheck == false) {
+        html = html.split("<!-- Projects -->")[0];
+    }
+
+
+    html = html + `
+        </div>
+    </div>
+</body>
+</html>`
 
     console.log(html)
     return false
