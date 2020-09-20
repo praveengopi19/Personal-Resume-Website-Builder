55262592647d2210a1a3c6fba578a1232a95011d
Praveen Kumar <praveengopi16@gmail.com>
2020-09-20 16:04:48 +0530

    img file type check and iteration without object

--- index.js
+++ index.js
@@ -1,24 +1,34 @@
-
-
 let skills = 1;
 let experience = 1;
 let education = 1;
 let projects = 1;
 
+var listobj = {
+    skills: [1],
+    education: [1],
+    experience: [1],
+    projects: [1]
+}
+
 let formvalues = document.getElementById("resume_form").elements;
-
-
 
 document.getElementById("click_div").addEventListener("click", async (event) => {
 
-    if (event.target.id === "delete" && event.target.nodeName === "BUTTON" && event.keyCode != 13) {
+    console.time("Time this");
+
+
+    if (event.target.id === "delete" && event.target.nodeName === "BUTTON") {
+        console.log(event.target.parentNode.parentNode.id)
         console.log(event.target.parentNode.remove())
+
         console.log("yes")
         event.stopPropagation();
         event.preventDefault();
     }
 
-    if (event.target.id === "addskills") {
+    //addskills
+
+    else if (event.target.id === "addskills") {
         skills = skills + 1;
         let template = `
                 <button type="button" id="delete">Delete</button>
@@ -33,10 +43,33 @@ document.getElementById("click_div").addEventListener("click", async (event) => 
 
         event.stopPropagation();
         event.preventDefault();
-
     }
 
-    if (event.target.id === "submitform" || event.target.id === "previewresume") {
+
+
+
+    //Submit
+
+    else if (event.target.id === "submitform" || event.target.id === "previewresume") {
+
+        let error = false
+
+        if (!formvalues["photo"].files[0]) {
+            return false
+        }
+
+        let extension = formvalues["photo"].files[0].type.split('/').pop().toLowerCase()
+
+        if (extension !== "jpeg" && extension !== "gif" && extension !== "png" && extension !== "jpg") {
+            console.log(extension)
+            return false
+        }
+
+        if (formvalues["photo"].files[0].size > 512000) {
+            console.log(formvalues["photo"].files[0].size / 1024)
+            return false
+        }
+
         let html = `<!DOCTYPE html>
 <html>
 
@@ -47,169 +80,8 @@ document.getElementById("click_div").addEventListener("click", async (event) => 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     <link href="./style.css" rel="stylesheet">`
 
-        let css = `
-    html,
-body {
-    margin            : 0;
-    padding           : 0;
-    -webkit-box-sizing: border-box;
-    box-sizing        : border-box;
-    font-family       : 'Catamaran', sans-serif;
-    letter-spacing    : 0.3px;
-    height            : 100%;
-}
-
-
-
-a {
-    text-decoration: none;
-}
-
-
-
-.left {
-    width     : 50%;
-    min-height: 100%;
-    float     : left;
-    text-align: center;
-    overflow  : hidden;
-    position  : fixed;
-
-}
-
-.leftbg {
-    background     : url(./images/web-image3.jpg) center center no-repeat;
-    background-size: cover;
-    height         : 100%;
-    width          : 100%;
-    position       : absolute;
-    top            : 0;
-    right          : 0;
-    bottom         : 0;
-    left           : 0;
-}
-
-.leftbg:before {
-    content         : '';
-    position        : absolute;
-    top             : 0;
-    right           : 0;
-    bottom          : 0;
-    left            : 0;
-    background      : #185a9d;
-    background-image: linear-gradient(to left top, #43cea2, #185a9d);
-    opacity         : 0.85;
-}
-
-.left_inner,
-.right_inner {
-    padding: 50px;
-}
-
-.left_inner {
-    min-height: 25em;
-    color     : white;
-    top       : 0;
-    text-align: center;
-    height    : 100%;
-}
-
-.left_info {
-    width   : 90%;
-    position: absolute;
-    top     : 0;
-    left    : 0;
-    right   : 0;
-    bottom  : 0;
-    margin  : 50vh auto 0 auto;
-}
-
-.links {
-    margin-top: 10px;
-}
-
-.fa {
-    color    : white;
-    margin   : 10px;
-    font-size: 24px;
-}
-
-
-.right {
-    float: right;
-    width: 50%;
-}
-
-.right_inner {
-    font-weight: 300;
-    line-height: 1.5;
-}
-
-.heading {
-    color           : #4169e1;
-    font-size       : 14px;
-    margin-block-end: 1em;
-}
-
-.subheading {
-    font-weight: 700;
-    padding    : 0 0 5px 0;
-}
-
-.content {
-    margin: 0 0 40px 0;
-}
-
-.category {
-    margin-bottom: 10px;
-}
-
-strong {
-    font-size: 14px;
-}
-
-.duration {
-    color: #63738b;
-}
-
-.right_inner h4:first-of-type {
-    margin-block-start: 0;
-}
-
-.pdf_link {
-    font-weight     : 600;
-    font-size       : 14px;
-    border-radius   : 5px;
-    display         : block;
-    background-color: #f5f5f5;
-    color           : #4169e1;
-    padding         : 10px;
-    text-align      : center;
-    width           : 100%;
-    max-width       : 500px;
-    margin          : 0 auto;
-}
-
-@media screen and (max-width: 1000px) {
-
-    .left,
-    .right {
-        width: 100%;
-    }
-
-    .left {
-        position: static;
-    }
-
-}`
         let img = new Image()
         img.src = window.URL.createObjectURL(formvalues["photo"].files[0])
-        img.onload = () => {
-            alert(img.width + img.height)
-            if (img.width !== img.height) {
-                return false
-            }
-        }
 
         html = html + `
         <title>Resume | ${formvalues["name"].value}</title>
@@ -217,8 +89,9 @@ strong {
     
     <body>
         <div class="left">
-            <div class="leftbg">
-            <img src =${img.src} </div>
+            <div class="leftbg"
+            style="background : url(${event.target.id === "submitform" ? `./${formvalues["photo"].files[0].name}` : `${img.src}`}) center center no-repeat !important; background-size: cover !important ">
+            </div>
             <div class="left_inner">
                 <div class="left_info">
             `;
@@ -286,7 +159,7 @@ strong {
             <div class="content">
             <h4 class="heading">INTRO</h4>
             <p>
-            ${formvalues["about"].value}
+            ${formvalues["about"].value.trim().replace(/\n/g, "</p>\n<p>")}
             </p>
         </div>`
         }
@@ -433,24 +306,22 @@ strong {
     </body>
     </html>`
 
+        //preview resume && submit form
+
         if (event.target.id === "previewresume") {
             localStorage.setItem("resume_online", JSON.stringify(html));
             window.open("preview.html")
         }
         else if (event.target.id === "submitform") {
 
-
-
             let style = await fetch('./style.css')
             style = await style.text()
-            console.log(style)
 
             var zip = new JSZip();
             zip.file("index.html", html);
-            zip.file("style.png", formvalues["photo"].files[0])
+            zip.file(formvalues["photo"].files[0].name, formvalues["photo"].files[0])
             zip.file("style.css", style)
 
-            // zip.add("style.css",);
             let content = await zip.generateAsync({ type: "base64" });
 
             // let element = document.createElement('a');
@@ -469,23 +340,13 @@ strong {
             element.click();
             document.body.removeChild(element)
 
-            // element = document.createElement('a');
-            // element.setAttribute('href', 'style.css')
-            // element.setAttribute('download', 'style.css')
-            // element.style.display = 'none'
-            // document.body.appendChild(element)
-            // element.click();
-            // document.body.removeChild(element)
         }
 
-        //let preview = document.getElementById("preview");
-        //preview.innerHTML = html;
-
-        console.log(html)
         event.stopPropagation();
         event.preventDefault();
     }
 
+    console.timeEnd("Time this");
 
 })
 
